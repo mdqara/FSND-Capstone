75828e137bdf05d5d446e4020c958c3622e37969
Mohammad Qara <mdqara@gmail.com>
2020-08-09 17:10:42 +0300

    working on db schema

--- application/routes.py
+++ application/routes.py
@@ -4,20 +4,24 @@ from models import Course, Instructor
 from models import db
 
 
-courseData = [{"courseID":"1111","title":"FSND","description":"Full Stack Nano Degree","credits":"3","term":"Fall, Spring"}, 
-              {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
-              {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
-              {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
-              {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+courseData = [{"courseID": "1111", "title": "FSND", "description": "Full Stack Nano Degree", "credits": "3", "term": "Fall, Spring"},
+              {"courseID": "2222", "title": "Java 1",
+                  "description": "Intro to Java Programming", "credits": "4", "term": "Spring"},
+              {"courseID": "3333", "title": "Adv PHP 201",
+                  "description": "Advanced PHP Programming", "credits": "3", "term": "Fall"},
+              {"courseID": "4444", "title": "Angular 1",
+                  "description": "Intro to Angular", "credits": "3", "term": "Fall, Spring"},
+              {"courseID": "5555", "title": "Java 2", "description": "Advanced Java Programming", "credits": "4", "term": "Fall"}]
 
 
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Headers',
-                            'Content-Type,Authorization,true')
+                         'Content-Type,Authorization,true')
     response.headers.add('Access-Control-Allow-Methods',
-                            'GET,PATCH,POST,DELETE,OPTIONS')
+                         'GET,PATCH,POST,DELETE,OPTIONS')
     return response
+
 
 @app.route("/")
 @app.route("/index")
@@ -28,7 +32,7 @@ def index():
 @app.route("/courses/")
 @app.route("/courses/<term>")
 def courses(term=2020):
-    return render_template("courses.html", courseData=courseData, courses = True )
+    return render_template("courses.html", courseData=courseData, courses=True)
 
 
 @app.route("/register")
@@ -40,9 +44,11 @@ def register():
 def login():
     return render_template('login.html', login=True)
 
+
 @app.route("/user")
 def user():
     return render_template('user.html')
+
 
 @app.route("/add-course")
 def add_course():
@@ -51,51 +57,49 @@ def add_course():
 
 @app.route("/create-course", methods=['POST'])
 def create_course():
-    try:
 
-        data = request.get_json
-        print(data)
-        
-        name            = request.form.get("name")
-        description     = request.form.get("description")
-        duration        = request.form.get("duration")
-        image_link      = request.form.get("image_link")
-    
-        course_to_add = Course(
-            name=name,
-            description=description,
-            duration=duration,
-            image_link=image_link,
-        )
+    # try:
+    course_name = request.form.get('course-name')
+    description = request.form.get('course-description')
+    duration = request.form.get('course-duration')
+    imgURL = request.form.get('course-img-URL')
 
-        print(course_to_add)
+    course_to_add = Course(
+        name=course_name,
+        description=description,
+        duration=duration,
+        image_link=imgURL,
+    )
 
-        db.session.add(course_to_add)
-        db.session.commit()
-
-    except:
-        db.session.rollback()
-    finally:
-        db.session.close()
+    db.session.add(course_to_add)
+    db.session.commit()
 
     return render_template("index.html")
 
 
-@app.route("/enrollment", methods=["GET","POST"])
+# except Exception as e:
+#    db.session.rollback()
+#    print(e)
+
+# finally:
+#    db.session.close()
+
+
+@app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
 
-    #body = request.get_json()
-    #data = body 
+    # body = request.get_json()
+    # data = body
     id = request.form.get('courseID')
     title = request.form.get('title')
     term = request.form.get('term')
-    return render_template("enrollment.html", enrollment=False, data={"id":id,"title":title,"term":term}) 
+    return render_template("enrollment.html", enrollment=False, data={"id": id, "title": title, "term": term})
 
 
-@app.route('/api/') 
-@app.route('/api/<index>') 
+@app.route('/api/')
+@app.route('/api/<index>')
 def api(index=None):
-    if(index==None):
+    if(index == None):
         json_data = courseData
     else:
         json_data = courseData[int(index)]
--- models.py
+++ models.py
@@ -7,6 +7,8 @@ db = SQLAlchemy()
 # ----------------------------------------------------------------------------#
 # App Config.
 # ----------------------------------------------------------------------------#
+
+
 def setup(app):
     app.config.from_object("config")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,41 +21,43 @@ def setup(app):
 # Models.
 # ----------------------------------------------------------------------------#
 
-class Student(db.Model):
-    __tablename__ = "student"
 
-    id                  = db.Column(db.Integer, primary_key=True)
-    fname               = db.Column(db.String(120))
-    lanme               = db.Column(db.String(120))
-    phone               = db.Column(db.String(120))
-    email               = db.Column(db.String(120))
-    enrollment          = db.relationship("enrollment", backref="student", cascade="all,delete", lazy=True)
+class Student(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(120))
+    lanme = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    enrollment = db.relationship(
+        "Enrollment ", backref="studentt", cascade="all,delete", lazy=True)
+
 
 class Course(db.Model):
-    __tablename__ = "course"
 
-    id                  = db.Column(db.Integer, primary_key=True)
-    name                = db.Column(db.String(120))
-    description         = db.Column(db.String(120))
-    duration            = db.Column(db.String(120))
-    image_link          = db.Column(db.String(500))
-    enrollment          = db.relationship("enrollment", backref="course", cascade="all,delete", lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(120))
+    duration = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    enrollment = db.relationship(
+        "Enrollment", backref="subject", cascade="all,delete", lazy=True)
 
 
 class Instructor(db.Model):
-    __tablename__ = "instructor"
 
-    id                  = db.Column(db.Integer, primary_key=True)
-    fname               = db.Column(db.String)
-    lanme               = db.Column(db.String(120))
-    phone               = db.Column(db.String(120))
-    email               = db.Column(db.String(120))
-    enrollment               = db.relationship("enrollment", backref="instructor", cascade="all,delete", lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String)
+    lanme = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    enrollment = db.relationship(
+        "Enrollment", backref="teacher", cascade="all,delete", lazy=True)
+
 
 class Enrollment(db.Model):
-    __tablename__ = "enrollment"
-    
-    id                  = db.Column(db.Integer, primary_key=True)
-    student_id          = db.Column(db.Integer, db.ForeignKey("student.id"))
-    course_id           = db.Column(db.Integer, db.ForeignKey("course.id"))
-    instructor_id       = db.Column(db.Integer, db.ForeignKey("instructor.id"))
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
+    instructor_id = db.Column(db.Integer, db.ForeignKey("instructor.id"))
